934ecda4e9049c211580af4402ce2ba17b6daf6c
88Jitendra <choudharyjitendra205@gmail.com>
2019-04-05 10:32:42 +0530

    add last day work

--- FrameExtract.py
+++ FrameExtract.py
@@ -1,21 +1,31 @@
 import cv2 
-
+import os
+import shutil
+from glob import glob
+folderCounter=1
+folderBase="FrameFolder
 
 def FrameCapture(path): 
       
     vidObj = cv2.VideoCapture(path)
     success = 1
-  
+    count=0
+    folderName=folderBase+str(folderCounter)
+    os.mkdir(folderName)
+    folderCounter+=1
     while success: 
         success, image = vidObj.read() 
-        cv2.imwrite("/home/aman/Desktop/Mini-Project/Frames/frame%d.jpg" % count, image) 
+	pathname=folderName+"/frame"+str(count)+'.jpg'
+        cv2.imwrite(pathname, image) 
   
         count += 1
 
 
 if __name__ == '__main__': 
-
-    FrameCapture("/home/aman/Desktop/Mini-Project/testVideo.mp4") 
+    
+    for var in glob("/home/aman/Desktop/Mini-Project/VideoClips/*.mp4"):
+    	FrameCapture(var) 
+shutil.rmtree("VideoClips")
 
 
 
